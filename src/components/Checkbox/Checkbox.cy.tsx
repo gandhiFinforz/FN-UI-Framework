4e2b20739a020fb1fd6d82b913eb77c15aa2471c
Sathish Kumar K <sathish@lockboxai.com>
2024-06-19 17:33:05 +0530

    writing on unit testing for checkbox component

---
+++ src/components/Checkbox/Checkbox.cy.tsx
@@ -0,0 +1,56 @@
+/// <reference types="cypress" />
+import { mount } from "@cypress/react";
+import FNCheckbox from "./Checkbox";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primeicons/primeicons.css"; // Import PrimeIcons for icon support
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
+});
+
+describe("<FNCheckbox />", () => {
+  it("renders with label", () => {
+    mount(
+      <FNCheckbox label="Remember Me" checked={false} onChange={() => {}} />
+    );
+    cy.get("label").contains("Remember Me").should("exist");
+  });
+
+  it("renders checked checkbox", () => {
+    mount(
+      <FNCheckbox label="Remember Me" checked={true} onChange={() => {}} />
+    );
+    cy.get("input.p-checkbox-input")
+      .should("exist")
+      .invoke("prop", "checked")
+      .should("eq", true);
+  });
+
+    it("calls onChange when checkbox is clicked", () => {
+      const handleChange = cy.stub().as("handleChange");
+  
+      mount(
+        <FNCheckbox label="Remember Me" checked={false} onChange={handleChange} />
+      );
+  
+      cy.get("input.p-checkbox-input")
+    .check()
+    .trigger('change'); // Trigger change event after checking
+
+  cy.get("@handleChange").should("have.been.calledWith", { checked: true });
+
+    });
+   
+
+  it("disables checkbox when disabled prop is true", () => {
+    mount(
+      <FNCheckbox
+        label="Remember Me"
+        checked={false}
+        disabled={true}
+        onChange={() => {}}
+      />
+    );
+    cy.get("input.p-checkbox-input").should("be.disabled");
+  });
+});
