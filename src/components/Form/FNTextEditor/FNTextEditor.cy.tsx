0ec238d1f2129c1e1e3620b558af6d65ced3bb63
Bhavani-RTSG <bhavani.mut@finforz.com>
2024-06-25 11:24:19 +0530

    naming chnages

--- src/components/Form/FNTextEditor/FNTextEditor.cy.tsx
+++ src/components/Form/FNTextEditor/FNTextEditor.cy.tsx
@@ -12,21 +12,23 @@ const renderHeader = () => {
 
 const header = renderHeader();
 
-describe("<FNTextArea />", () => {
+describe("<FNTextEditor />", () => {
   it("renders", () => {
     mount(<FNTextEditor value={""} />);
-    cy.screenshot("component/FNTextArea/renders");
+    cy.screenshot("component/FNTextEditor/renders");
   });
   it("should allow text input", () => {
     mount(<FNTextEditor value={""} />);
     const newText = "New text content";
     cy.get(".ql-editor").clear().type(newText);
     cy.get(".ql-editor").should("contain.text", newText);
+    cy.screenshot("component/FNTextEditor/renders");
   });
 
   it("should apply custom height correctly", () => {
     mount(<FNTextEditor value={""} height="500px" />);
     cy.get(".ql-container").should("have.css", "height", "500px");
+    cy.screenshot("component/FNTextEditor/renders");
   });
   it("should render with custom header template", () => {
     mount(<FNTextEditor value={""} headerTemplate={header} />);
@@ -34,11 +36,13 @@ describe("<FNTextArea />", () => {
     cy.get(".ql-bold").should("exist");
     cy.get(".ql-italic").should("exist");
     cy.get(".ql-underline").should("exist");
+    cy.screenshot("component/FNTextEditor/renders");
   });
 
   it("should check content is not editable while readonly", () => {
     mount(<FNTextEditor value={""} readOnly />);
     cy.get(".ql-editor").should("have.attr", "contenteditable", "false");
+    cy.screenshot("component/FNTextEditor/renders");
   });
 
   it("should allow formatting with bold, italic, and underline", () => {
@@ -54,5 +58,6 @@ describe("<FNTextArea />", () => {
     cy.get(".ql-underline").click();
     cy.get(".ql-editor").type("Underlined text");
     cy.get(".ql-editor").find("u").should("contain.text", "Underlined text");
+    cy.screenshot("component/FNTextEditor/renders");
   });
 });
