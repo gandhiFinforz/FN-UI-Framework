84ef89cd8d4b54600b0802cc31a21dc5460373f4
gandhiFinforz <gandhi.sel@ascertain.com.my>
2024-06-26 14:18:01 +0530

    Config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'FN-UI-Framework',
-  webDir: 'build'
+  webDir: 'dist'
 };
 
 export default config;
