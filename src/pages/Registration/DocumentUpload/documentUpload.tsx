05952cca66f9a55d111b09f829205306dbee3508
Bhavani-RTSG <bhavani.mut@finforz.com>
2024-07-11 11:54:18 +0530

    height changes

--- src/pages/Registration/DocumentUpload/documentUpload.tsx
+++ src/pages/Registration/DocumentUpload/documentUpload.tsx
@@ -5,8 +5,8 @@ import "./documentUpload.css";
 
 const DocumentUpload: React.FC = () => {
   return (
-    <div className="grid justify-content-center" style={{ height: "90vh" }}>
-      <div className="col-12 sm:col-8 md:col-6 flex flex-column">
+    <div className="grid justify-content-center h-90vh  mt-2">
+      <div className="col-12 sm:col-8 md:col-5 flex flex-column">
         <h3 className="text-center">Document Upload</h3>
         <FNFileUpload name={""} mode={"advanced"} />
         <div className="mt-auto">
